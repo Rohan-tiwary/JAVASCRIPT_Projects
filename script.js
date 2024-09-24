02b0af1637953299e0b91a88fd304da1cde5a3bd
Rohan <rohankumartuiwary1406@gmail.com>
2024-09-24 19:45:01 +0530

    added feature2

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-var istatus=document.querySelector("h5");
-var addFriend=document.querySelector("#add");
-var check=0;
 
-addFriend.addEventListener("click",function(){
-    if(check==0){
-        istatus.innerHTML="Friends";
-        istatus.style.color="green";
-        addFriend.innerHTML="Remove"
-        check=1;
-    }else{
-       istatus.innerHTML="Stranger";
-       istatus.style.color="red";
-       addFriend.innerHTML="ADD Friend";
-       check=0;
-    }
-})
