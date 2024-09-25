5c85b54ea13544f06bdeceed0d5f7d89b30e5071
Rohan <rohankumartuiwary1406@gmail.com>
2024-09-25 19:24:36 +0530

    Added feature2

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+var con = document.querySelector("#container");
+var love = document.querySelector("i");
+
+con.addEventListener("dblclick", function() {
+    // Make the heart appear by scaling up
+    love.style.transform = 'translate(-50%, -50%) scale(1.5)';
+    love.style.opacity = 0.8;
+
+    // Fade out after 1 second
+    setTimeout(function() {
+        love.style.opacity = 0;
+        love.style.transform = 'translate(-50%, -50%) scale(0)';
+    }, 1000);
+});
 
